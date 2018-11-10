4def291bc94941118b1bf72a1c545ffb2be00e36
Caleb Rortvedt <carortv@gmail.com>
2018-11-10 15:31:50 -0600

    created item table

--- myFile.py
+++ myFile.py
@@ -1,1 +1,13 @@
-my file
+import sqlite3
+
+conn = sqlite3.connect('./userDatabase.db')
+
+with conn:
+    conn.execute("""CREATE TABLE IF NOT EXISTS item (
+        name TEXT,
+        type TEXT,
+        subtype TEXT,
+        date TEXT
+    )""")
+
+    conn.execute('INSERT INTO item (name, type, subtype, date) VALUES ("Vijay", "Clothes",  "Coat", "Today")')
