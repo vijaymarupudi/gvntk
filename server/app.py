791055d747f51e4dd3444f2da8364221155bf511
Caleb Rortvedt <carortv@gmail.com>
2018-11-10 17:45:02 -0600

    Did stuff

--- server/app.py
+++ server/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
