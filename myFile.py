e41de31b6b0a45f53afc2b8038de63103f9ead64
Hamza Ehsan <hamza.ehsan@lawrence.edu>
2018-11-10 15:24:28 -0600

    added user table sqlite

--- myFile.py
+++ myFile.py
@@ -1,1 +1,18 @@
-my file
+import sqlite3
+
+conn = sqlite3.connect('./test_database.db')
+
+id = 0
+
+with conn:
+    conn.execute("""CREATE TABLE IF NOT EXISTS user (
+        name TEXT,
+        type TEXT,
+        location TEXT,
+        email TEXT,
+        id INT
+        )""")
+    
+    conn.execute('INSERT INTO user (name, type, location, email, id) VALUES ("Hamza", "giver", "madison, Wi", "hamza.ehsan@lawrence.edu", 1 )')
+
+conn.close()
