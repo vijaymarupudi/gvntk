6b2abe6dc9601dbd26a79bb928f62e958f3da74e
Caleb Rortvedt <carortv@gmail.com>
2018-11-10 22:40:04 -0600

    Trying to get data to save in table

--- server/database_tools.py
+++ server/database_tools.py
@@ -2,6 +2,55 @@ import sqlite3
 
 conn = sqlite3.connect("./userDatabase.db")
 
+with conn:
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS item (
+        name TEXT,
+        mainCatagory TEXT,
+        subCatagory TEXT,
+        photo BLOB,
+        description TEXT,
+        timeCreated TEXT,
+        typeItem TEXT,
+        ownerEmail TEXT,
+        id INT
+    )"""
+    )
+
+# name item name
+# type of item i.e clothes
+# subtype is subtype of item i.e coat
+# photo is a picture of the item
+# explaination of item
+# timecreated is when the item was created
+# typeitem is either a give or take
+# owner is the user email who posted the item
+
+
+with conn:
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS user (
+        userName TEXT,
+        password TEXT,
+        accountType TEXT,
+        location TEXT,
+        email TEXT
+        )"""
+    )
+
+with conn:
+    # feedback contains feed back from the user
+    # timecreated is the time of the feedback posting
+    # creates the feedback schema
+
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS feedback (
+            feedback TEXT,
+            timeCreated TEXT,
+            feedBackName TEXT
+            )"""
+    )
+
 
 def itemCreation(
     name, mainCatagory, subCatagory, date, description, timeCreated, email, typeItem
@@ -30,6 +79,7 @@ def makeUser(userName, password, accountType, location, email):
             "INSERT INTO user (userName, password, accountType, location, email) VALUES (?,?,?,?,?)",
             (userName, password, accountType, location, email),
         )
+        conn.commit()
 
 def makeFeedback(feedback,timeCreated,feedBackName):
     
@@ -47,61 +97,4 @@ def returnItems(mainCatagory, typeItem):
 
     return listOfItems
 
-
-with conn:
-    conn.execute(
-        """CREATE TABLE IF NOT EXISTS item (
-        name TEXT,
-        mainCatagory TEXT,
-        subCatagory TEXT,
-        photo BLOB,
-        description TEXT,
-        timeCreated TEXT,
-        typeItem TEXT,
-        ownerEmail TEXT,
-        id INT
-    )"""
-    )
-
-# name item name
-# type of item i.e clothes
-# subtype is subtype of item i.e coat
-# photo is a picture of the item
-# explaination of item
-# timecreated is when the item was created
-# typeitem is either a give or take
-# owner is the user email who posted the item
-
-itemName = "Burei Watch"
-mainCatagory = "accesories"
-subCatagory = "wrists"
-date = "today"
-typeItem = "give"
-description = "a black watch with mesh steel band"
-timeCreated = "now"
-
-with conn:
-
-    conn.execute(
-        """CREATE TABLE IF NOT EXISTS user (
-        userName TEXT,
-        password TEXT,
-        accountType TEXT,
-        location TEXT,
-        email TEXT
-        )"""
-    )
-
-with conn:
-    #feedback contains feed back from the user
-    #timecreated is the time of the feedback posting
-    #creates the feedback schema
-    
-    conn.execute(
-                 """CREATE TABLE IF NOT EXISTS feedback (
-                     feedback TEXT,
-                     timeCreated TEXT,
-                     feedBackName TEXT
-                     )"""
-                 )
-
+print (conn.execute('SELECT * FROM user').fetchall())
