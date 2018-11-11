0a0a819b4a2ef5580e16caa863cf70f7bb618f0c
Vijay Marupudi <vijay@vijaymarupudi.com>
2018-11-10 22:42:13 -0600

    Added ModalButton

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import "../style.css";
 import MainImageFile from "../images/handpic.png";
@@ -20,15 +21,55 @@ function DisplayedImage(props) {
   );
 }
 
+function Modal(props) {
+  const { displayed, onClose } = props;
+
+  return (
+    <div className={["modal", displayed ? "is-active" : null].join(" ")}>
+      <div className="modal-background" onClick={onClose} />
+      <div className="modal-content">
+        <div className="box">{props.children}</div>
+      </div>
+      <button className="modal-close is-large" aria-label="close" />
+    </div>
+  );
+}
+
+class ModalMainButton extends Component {
+  state = {
+    modalOpen: false
+  };
+
+  toggleModal() {
+    this.setState({
+      modalOpen: !this.state.modalOpen
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <a className="mainbuttons" onClick={this.toggleModal.bind(this)}>
+          {this.props.text}
+        </a>
+        <Modal
+          displayed={this.state.modalOpen}
+          onClose={this.toggleModal.bind(this)}
+        >
+          {this.props.modalContent}
+        </Modal>
+      </>
+    );
+  }
+}
+
 class GiveOrTakeButtons extends Component {
   render() {
     return (
       <div className="section">
         <Centered>
-          <Link className="mainbuttons" to="/new_user">
-            I Need Something
-          </Link>
-          <Link to="/new_user" className="mainbuttons">I Have Something</Link>
+          <ModalMainButton text="I Want Something" modalContent={<p>Test</p>} />
+          <ModalMainButton text="I Need Something" modalContent={<p>Test</p>} />
         </Centered>
       </div>
     );
@@ -38,14 +79,14 @@ class ForUsButtons extends Component {
   render() {
     return (
       <div
-        class="section"
+        className="section"
         style={{
           marginTop: "100px"
         }}
       >
         <Centered>
-          <button class="forusbuttons">Our Mission</button>
-          <button class="forusbuttons">Feedback</button>
+          <button className="forusbuttons">Our Mission</button>
+          <button className="forusbuttons">Feedback</button>
         </Centered>
       </div>
     );
@@ -75,7 +116,7 @@ class App extends Component {
     return (
       <div>
         <div className="section">
-          <h1 class="title is-1">giveNtake</h1>
+          <h1 className="title is-1">giveNtake</h1>
         </div>
         <DisplayedImage image={MainImageFile} />
         <GiveOrTakeButtons />
