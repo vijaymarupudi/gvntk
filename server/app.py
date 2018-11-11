7df7bf7f2379e8bd5c7597ceecfceebea4a553af
Vijay Marupudi <vijay@vijaymarupudi.com>
2018-11-10 18:07:27 -0600

    Allow cross origin requests.

--- server/app.py
+++ server/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 
 @app.after_request
 def add_header(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
     return response
 
 
 @app.route("/")
+def index():
+    return redirect('http://127.0.0.1:3000')
+
+@app.route('/new_user', methods=["POST"])
 def hello():
-    return "Hello World!"
+    print('GOT POST')
+    return "Test"
+
 app.run()
