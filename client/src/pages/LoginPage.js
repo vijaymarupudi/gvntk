811862c9d4053cbe9892d5b3eb50869fbe365dc1
Vijay Marupudi <vijay@vijaymarupudi.com>
2018-11-11 10:02:57 -0600

    Fix merge conflict

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import * as forms from "../components/forms";
 import * as utils from "../utils";
-import {Redirect} from "react-router-dom"
+import { Redirect } from "react-router-dom";
 
 import Layout from "../components/Layout";
 
-function handleSubmit(e) {
-  e.preventDefault();
-  const formData = utils.formToObject(e.target);
-  utils.postData("/login", formData);
-  //<Redirect to="/new_item" />
-}
+export default class Login extends Component {
 
-export default function Login() {
-  return (
-    <Layout>
-      <div className="section container">
-        <form onSubmit={handleSubmit}>
-          <forms.TextField name="email" label="Email" />
-          <forms.TextField name="password" label="Password" type="password" />
-          <forms.Submit />
-        </form>
-      </div>
-    </Layout>
-  );
+  state = {
+    redirect: false
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const formData = utils.formToObject(e.target);
+    utils.postData("/login", formData);
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="section container">
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <forms.TextField name="email" label="Email" />
+            <forms.TextField name="password" label="Password" type="password" />
+            <forms.Submit />
+          </form>
+          {this.state.redirect && <Redirect to="/new_item" />}
+        </div>
+      </Layout>
+    );
+  }
 }
